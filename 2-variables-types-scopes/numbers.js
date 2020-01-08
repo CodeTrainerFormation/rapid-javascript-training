c848a640827b8eecad655291084e36627860f5ec
TanSoloo <tanneguy.jullin@gmail.com>
2020-01-08 16:42:08 -0500

    end of module 2 and module 3 Operators

--- 2-variables-types-scopes/numbers.js
+++ 2-variables-types-scopes/numbers.js
@@ -1,8 +1,8 @@
-let quantity = 2;
-let price = 0.99;
+// let quantity = 2;
+// let price = 0.99;
 
-console.log(quantity * price);
-console.log(typeof price);
+// console.log(quantity * price);
+// console.log(typeof price);
 
 
 // let oct = 071; // octo
@@ -23,7 +23,30 @@ let dec2 = 3.2e-4 // e- = puissance de -10 =  0.00032
 console.log(dec2);
 
 
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE * 2);
+
+let n = Infinity;
+console.log(typeof n);
+
+let n2 = -Infinity;
+console.log(typeof n2);
+
+console.log(Number.POSITIVE_INFINITY + ' ' + Number.NEGATIVE_INFINITY);
 
 
 
+let count;
+let price = 5.00;
 
+console.log(price / count);
+
+console.log(typeof NaN); // is a number
+
+let n3 = NaN;
+if (isNaN(n3))
+    console.log("Nan!"); // true
+else
+    console.log("is a Number!");
